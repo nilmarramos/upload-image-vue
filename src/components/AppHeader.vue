2295febd7e6c08a86f5e20c7cb047b0ede24a6f7
nilmar <nilmar@iMac-de-nilmar.local>
2018-07-12 13:01:53 -0300

    add pages

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,10 +1,15 @@
 <template>
 	<div class="ui secondary pointing menu">
-		<a href="/" class="active item">
+		<router-link to="/" class="active item">
 			Image Storage
-		</a>
+		</router-link>
 		<div class="right menu">
-			<a href="#" class="ui item" @click="login">
+			<div v-if="isLoggedIn" class="horizontal">
+				<router-link to="/" class="item">Galleries</router-link>
+				<router-link to="/upload" class="item">Upload</router-link>
+				<a class="item" @click="logout">Logout</a>
+			</div>
+			<a v-else href="#" class="ui item" @click="login">
 				Login
 			</a>
 		</div>
@@ -12,14 +17,17 @@
 </template>
 
 <script>
-	import { mapActions } from 'vuex'
+	import { mapActions, mapGetters } from 'vuex'
 
 	export default {
 		name: "AppHeader",
-		methods: mapActions(['login'])
+		methods: mapActions(['login','logout']),
+		computed: mapGetters(['isLoggedIn'])
 	}
 </script>
 
 <style scoped>
-
+	.horizontal {
+		display: flex;
+	}
 </style>
--- src/router.js
+++ src/router.js
@@ -2,12 +2,22 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import AuthHandler from './components/AuthHandler'
+import ImageList from './components/ImageList'
+import UploadForm from './components/UploadForm'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
+    {
+      path: '/',
+      component: ImageList
+    },
+    {
+      path: '/upload',
+      component: UploadForm
+    },
     {
       path: '/oauth2/callback',
       component: AuthHandler
